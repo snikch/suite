98d2d6e0d597b18a0a08ec851bfa7dfee8e70927
Mal Curtis <mal@mal.co.nz>
2012-03-04 08:13:32 +1300

    Add project root to Suite

--- lib/suite.rb
+++ lib/suite.rb
@@ -4,4 +4,13 @@ require "suite/cli"
 
 module Suite
   include Suite::Core
+  class << self
+    def project_path= name
+      @@project_path = name
+    end
+
+    def project_path
+      @@project_path
+    end
+  end
 end
