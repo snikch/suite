7bd7cbe2fce22c4cfbc7b857079f5cffa870e688
Mal Curtis <mal@mal.co.nz>
2012-03-04 08:29:59 +1300

    Remove helper path references

--- lib/suite/server/asset.rb
+++ lib/suite/server/asset.rb
@@ -1,11 +1,9 @@
-require 'suite/helpers/path'
 require 'suite/server/error'
 require 'mime/types'
 
 module Suite
   class AssetServer < Goliath::API
     include Suite::ServerError
-    include Suite::Helpers::Path
     def response(env)
       file = Suite.project_path + "/assets/" + env.params[:asset].join("/")
       return not_found unless File.exists? file
