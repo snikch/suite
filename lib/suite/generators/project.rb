02d84daaf309a108d87da0f750b7cafec00d4811
Mal Curtis <mal@sitepoint.com>
2012-03-05 21:36:31 +1300

    Added prompts for config options in project generator

--- lib/suite/generators/project.rb
+++ lib/suite/generators/project.rb
@@ -1,9 +1,22 @@
+# encoding: utf-8
 require 'thor/group'
 module Suite
   module Generators
     class Project < Thor::Group
       include Thor::Actions
       argument :name, type: :string
+
+      def create_config
+        say "I’m going to ask you some questions to create your config file"
+        say "You can change these values in your config/suite.yml file"
+        @compress_javascript = yes? "Compress JavaScript? [Yn]", :blue
+        @shorten_javascript_variables = yes? "Shorten JavaScript Variables? [Yn]", :blue
+        @compress_stylesheet = yes? "Compress CSS? [Yn]", :blue
+        @using_cdn = yes? "Will you serve assets from a CDN?", :blue
+        if @using_cdn
+          @asset_host = ask "CDN Host and path [e.g. http://ak43nam.cloudfront.net/new_site]", :blue
+        end
+      end
 
       def create_directory_structure
         empty_directory
@@ -18,7 +31,7 @@ module Suite
       def copy_files
         template "application.js.erb", "#{name}/assets/javascripts/application.js"
         template "application.scss.erb", "#{name}/assets/stylesheets/#{name}.css.scss"
-        template "suite.yml", "#{name}/config/suite.yml"
+        template "suite.yml.erb", "#{name}/config/suite.yml"
         template "content.yml", "#{name}/config/content.yml"
       end
 
